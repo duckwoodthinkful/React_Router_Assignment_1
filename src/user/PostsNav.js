13661d99f645dd6620003c768cd021ea15f0a1be
Abe Krebs <duckwood@gmail.com>
2021-04-11 10:36:11 -0400

    first commit

---
+++ src/user/PostsNav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
+
+
+// TODO: Change the link below to go back to the home page.
+
+export const PostsNav = () => (
+  <nav aria-label="breadcrumb">
+    <ol className="breadcrumb">
+      <li className="breadcrumb-item">
+        <Link to="/" className="btn btn-link">Go Home</Link>
+      </li>
+    </ol>
+  </nav>
+);
+
+export default PostsNav;
